eae870b0a00c0b4bd238d31fc117b04f7fd722ab
Sandra Suurkask <sarascono@gmail.com>
2023-03-03 15:58:39 +0200

    Add item list example

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,24 @@
 <script>
+  let counter = 0;
+  let message = 'Hello svelte';
+  let items = ['piim', 'sai', 'viin', 'siider']
+
+function add(){
+  items.push(message);
+  items = items;
+  message = "";
+}
 
 </script>
 
-<button class="button is primary">Click me</button>
-
-<style>
-  
-</style>
+<button class="button is primary" on:click={() => counter++}>Value is {counter}</button>
+<div class="content">
+  <input type="text" class="input" bind:value={message}>
+  <button class="button is primary" on click={}>Add</button>
+  <h1>{message}</h1>
+  <ul>
+    {#each items as item}
+    <li>{items}</li>
+    {/each}
+  </ul>
+</div>
